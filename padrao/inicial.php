7cf733bbff1932a55b46515ae209f41fabb863d6
DiomarBR <diomarbr4@gmail.com>
2022-04-01 00:48:05 -0300

    comiti incial

---
+++ padrao/inicial.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+    <p>ola mundo</p>
+    <body>
+    </body>
+</html>
---
+++ unidade06/inicial.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+        <p><?php 
+            // comentario em php
+            echo "Diomar Gonçalves";
+            ?>
+        </p>
+        <p>ola mundo</p>
+        <?php echo 3*3;?>
+        <!---ola mundo--->
+    </body>
+</html>
